67fcb5f8e29ed4f4e6702d66ba2fa5c33731cef2
mmaedee <maedeezorbaji@gmail.com>
2023-12-15 13:45:00 +0330

    Add project files.

---
+++ GetPersonInfo/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PersonInfo;
+
+
+
+string information = "maede , tehran , 22;maryam , tehran , 30;helia , shiraz , 21;yasmin , tabriz , 20";
+
+PersonInformation personInfo = new PersonInformation();
+
+List<string> result = personInfo.GetEachPersonInfo(personInfo.GetInfo(information));
+var res = result.ToArray();
+
+for (int i = 0; i < res.Length; i++)
+{
+    Console.WriteLine(res[i]);
+}
+
+var personCount = personInfo.GetInfo(information);
+
+int sumAge = personInfo.CalculateSumAge(result);
+//Console.WriteLine(sumAge);
+
+int avgAge = personInfo.CalculateAvgAge(sumAge, personCount.Count());
+
+Console.WriteLine($"{avgAge}");
+
+Console.ReadKey();
+
+
---
+++ PersonInfo/PersonInformation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    public class PersonInformation
+    {
+
+        public string[] GetInfo(string information)
+        {
+            string[] newInfo = information.Split(";");
+
+            return newInfo;
+        }
+
+
+        public List<string> GetEachPersonInfo(string[] eachPersonInfo)
+        {
+
+            string[] splitedInfo = new string[3];
+            List<string> newInfo = new List<string>();
+
+            foreach (string eachPerson in eachPersonInfo)
+            {
+                splitedInfo = eachPerson.Split(",");
+                newInfo.AddRange(splitedInfo);
+            }
+
+            return newInfo;
+        }
+
+        public int CalculateSumAge(List<string> info)
+        {
+            int age = 0;
+            string[] infoArray = info.ToArray();
+
+            for (int i = -1; i < infoArray.Length; i += 3)
+            {
+                while (i >= 2)
+                {
+                    string convertedAge = infoArray[i].ToString();
+                    age += Convert.ToInt32(convertedAge);
+                    break;
+                }
+
+            }
+            return age;
+
+        }
+
+        public int CalculateAvgAge(int sumAge, int personCount)
+        {
+            int avgAge = sumAge / personCount;
+            return avgAge;
+        }
+
+    }
+}
